b88ea6ec47bbe51b4dc8e766ac84b103ac01fd5b
VitorGiammella <vitor.giammella@soaexpert.com>
2017-02-17 16:15:47 -0200

    firebase

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,7 +1,36 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  nome :'nooome',
-  sobrenome: 'thicken',
-  foods:['Tacos','Salad','Vegetables','Fruit']
+  name :'Renan',
+  sobrenome: 'favoriteFood',
+
+  estaurant:{nome: 'Jurassik', yearOpen: 1},
+
+  actions:{
+    wasClicked(elementName){
+      alert(' was clicled')
+    },
+    makeUnavaliable(food){
+      Ember.set(food, 'isAvaliable', false)
+    },
+    makeAvaliable(food){
+      Ember.set(food, 'isAvaliable', true)
+    }
+  }
 });
+/*
+Example:
+
+function tacoAlert(adjective, tooping){
+  alert('Tacos are' + adjective + '!');
+  alert('Melhor ainda com' + tooping);
+}
+
+function tacoString(adjective, tooping){
+  return 'Tacos are' + adjective + '. Melhor ainda com' + tooping;
+}
+
+tacoAlert('bons','leite')
+OU
+tacoAlert(tacoString('bons','leite'))
+*/
--- app/routes/application.js
+++ app/routes/application.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model(){
+    return[
+    {name:'Tacos', isAvaliable: true},
+    {name:'Salad', isAvaliable: true},
+    {name:'Vegetables', isAvaliable: false},
+    {name:'Fruit', isAvaliable: true}
+    ]
+  }
 });
